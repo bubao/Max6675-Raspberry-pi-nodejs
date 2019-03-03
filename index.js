c624ddcc044b7548fd5b23826d99a6e3b4f668ce
bubao <asd565586630@gmail.com>
2019-03-03 15:40:03 +0800

    error

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ module.exports = class Max6675 {
 		this.cs.writeSync(1);
 
 		const results = this[format](value, this.UNIT);
-
+		console.log(error_tc)
 		if (error_tc !== 0) return new Error("error: can't get temp");
 		return results;
 	}
