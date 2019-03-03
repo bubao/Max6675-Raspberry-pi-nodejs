59da639406dc91c8aab19dec81846ceb6a333df9
bubao <asd565586630@gmail.com>
2019-03-03 15:11:35 +0800

    change gpio

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const Max6675 = require("./index");
 const CS = 4;
 const SCK = 24;
 // const SO = 25;
-const SO = [25];
+const SO = [12, 16, 20, 21];
 
 (async () => {
 	const max = new Max6675(CS, SCK, SO, 1);
